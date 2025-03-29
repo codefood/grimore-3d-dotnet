fc522eab04565a5913b7e0eceae5aac4ed11bc8d
Lawrence Anstey <lawrence.anstey@gmail.com>
2025-03-29 20:10:49 +0000

    manager class

--- World.cs
+++ World.cs
@@ -7,61 +7,7 @@ namespace grimore3ddotnet;
 
 public partial class World : Node3D
 {
-	[Export]
-	private PackedScene _tileScene = ResourceLoader.Load<PackedScene>("res://tile.tscn");
-
-	[Export] 
-	private PackedScene _wallScene = ResourceLoader.Load<PackedScene>("res://wall.tscn");
-	private const int TileSize = 2;
-	private int _offsetRows;
-	private int _offsetCols;
-
-
-	public Dictionary<PackedScene, char[]> GetThings() => new()
-	{
-		{ _tileScene, ['X', 'x', '.'] },
-		{ _wallScene, ['W', 'w'] }
-	};
-	
-	private void Load(string level)
-	{
-		var levelLines = level.Split('\n');
-		var things = GetThings();
-		
-		var width = levelLines.Max(l => l.Length);
-		var height = levelLines.Length;
-		_offsetRows = 0 - width / 2;
-		_offsetCols = 0 - height / 2;
-		
-		for (var row = 0; row < height; row++)
-		for (var col = 0; col < width; col++)
-		{
-			var thing = things
-				.FirstOrDefault(t =>
-					t.Value.Any(c => c == levelLines[row].ElementAtOrDefault(col)))
-				.Key;
-			
-			if (thing != null)
-			{
-				AddThing(row, col, thing);
-			}
-		}
-			
-	}
-	
-	private void AddThing(int row, int col, PackedScene scene)
-	{
-		var thing = scene.Instantiate() as Node3D;
-		thing!.Position = new Vector3((col + _offsetCols) * TileSize, 0, (row + _offsetRows) * TileSize);
-		AddChild(thing);
-	}
-
-	private void ClearThings()
-	{
-		GetChildren().OfType<Tile>().ForEach(x => x.QueueFree());
-		GetChildren().OfType<Wall>().ForEach(x => x.QueueFree());
-	}
-	
+	private readonly WorldManager _manager = new();
 	private const float Speed = 1;
 
 	public override void _Input(InputEvent ev)
@@ -73,7 +19,7 @@ public partial class World : Node3D
 
 		if (ev.IsActionPressed(Actions.Clear))
 		{
-			ClearThings();
+			_manager.ClearThingsFrom(this);
 		}
 	}
 
@@ -93,12 +39,9 @@ public partial class World : Node3D
 	}
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		ClearThings();
-		Load(Levels.Three);
-	}
-	
+	public override void _Ready() => 
+		_manager.LoadLevel(this, Levels.Three);
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
@@ -107,7 +50,7 @@ public partial class World : Node3D
 					? action.Value
 					: Vector2.Zero)
 			.ToList();
-		var totalDirection = new Vector3(directionVectors.Sum(v => v.X) * TileSize, 0, directionVectors.Sum(v => v.Y) * TileSize);
+		var totalDirection = new Vector3(directionVectors.Sum(v => v.X) * _manager.TileSize, 0, directionVectors.Sum(v => v.Y) * _manager.TileSize);
 		if (totalDirection != Vector3.Zero)
 		{
 			MovePlayer(totalDirection);
---
+++ WorldManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+namespace grimore3ddotnet;
+
+public class WorldManager
+{
+    public int TileSize => 2;
+    private int _offsetRows;
+    private int _offsetCols;
+    
+    static readonly Dictionary<PackedScene, char[]> Things = new()
+    {
+        { ResourceLoader.Load<PackedScene>("res://tile.tscn"), ['X', 'x', '.'] },
+        { ResourceLoader.Load<PackedScene>("res://wall.tscn"), ['W', 'w'] }
+    };
+
+    public void LoadLevel(World world, string level)
+    {
+        ClearThingsFrom(world);
+        var levelLines = level.Split('\n');
+
+        var width = levelLines.Max(l => l.Length);
+        var height = levelLines.Length;
+        _offsetRows = 0 - width / 2;
+        _offsetCols = 0 - height / 2;
+
+        for (var row = 0; row < height; row++)
+        for (var col = 0; col < width; col++)
+        {
+            var thing = Things
+                .FirstOrDefault(t =>
+                    t.Value.Any(c => c == levelLines[row].ElementAtOrDefault(col)))
+                .Key;
+
+            if (thing == null) continue;
+            
+            var instance = thing.Instantiate() as Node3D;
+            instance!.Position = new Vector3((col + _offsetCols) * TileSize, 0, (row + _offsetRows) * TileSize);
+            world.AddChild(instance);
+        }
+    }
+
+    public void ClearThingsFrom(World world)
+    {
+        world.GetChildren().OfType<Tile>().ForEach(x => x.QueueFree());
+        world.GetChildren().OfType<Wall>().ForEach(x => x.QueueFree());
+    }
+}
