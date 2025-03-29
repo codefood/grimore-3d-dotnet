6976389786773cdb44794e1bea030cf15c1d0de9
Lawrence Anstey <lawrence.anstey@gmail.com>
2025-03-29 20:01:56 +0000

    building world from dict

--- World.cs
+++ World.cs
@@ -8,20 +8,25 @@ namespace grimore3ddotnet;
 public partial class World : Node3D
 {
 	[Export]
-	private PackedScene _tileScene = GD.Load<PackedScene>("res://tile.tscn");
+	private PackedScene _tileScene = ResourceLoader.Load<PackedScene>("res://tile.tscn");
 
 	[Export] 
-	private PackedScene _wallScene = GD.Load<PackedScene>("res://wall.tscn");
-	
-	private const float Speed = 1;
+	private PackedScene _wallScene = ResourceLoader.Load<PackedScene>("res://wall.tscn");
 	private const int TileSize = 2;
-
 	private int _offsetRows;
 	private int _offsetCols;
+
+
+	public Dictionary<PackedScene, char[]> GetThings() => new()
+	{
+		{ _tileScene, ['X', 'x', '.'] },
+		{ _wallScene, ['W', 'w'] }
+	};
 	
 	private void Load(string level)
 	{
 		var levelLines = level.Split('\n');
+		var things = GetThings();
 		
 		var width = levelLines.Max(l => l.Length);
 		var height = levelLines.Length;
@@ -30,23 +35,24 @@ public partial class World : Node3D
 		
 		for (var row = 0; row < height; row++)
 		for (var col = 0; col < width; col++)
-
-			switch (levelLines[row].ElementAtOrDefault(col))
+		{
+			var thing = things
+				.FirstOrDefault(t =>
+					t.Value.Any(c => c == levelLines[row].ElementAtOrDefault(col)))
+				.Key;
+			
+			if (thing != null)
 			{
-				case 'W' or 'w':
-					AddThing(row, col, _wallScene);
-					break;
-				case 'X' or 'x':
-					AddThing(row, col, _tileScene);
-					break;
-			};
+				AddThing(row, col, thing);
+			}
+		}
+			
 	}
 	
 	private void AddThing(int row, int col, PackedScene scene)
 	{
 		var thing = scene.Instantiate() as Node3D;
 		thing!.Position = new Vector3((col + _offsetCols) * TileSize, 0, (row + _offsetRows) * TileSize);
-		GD.Print($"instantiating {thing.Name} at {thing.Position}");
 		AddChild(thing);
 	}
 
@@ -56,11 +62,13 @@ public partial class World : Node3D
 		GetChildren().OfType<Wall>().ForEach(x => x.QueueFree());
 	}
 	
+	private const float Speed = 1;
+
 	public override void _Input(InputEvent ev)
 	{
 		if (ev.IsActionPressed(Actions.Act))
 		{
-			MovePlayer(Vector3.Up);
+			//todo: an action
 		}
 
 		if (ev.IsActionPressed(Actions.Clear))
