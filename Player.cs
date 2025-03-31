6891e08181a169e7a9da28651d8700a5a9351d41
Lawrence Anstey <lawrence.anstey@gmail.com>
2025-03-31 19:19:51 +0100

    ok now the camera works

--- Player.cs
+++ Player.cs
@@ -8,14 +8,14 @@ public partial class Player : CharacterBody3D
 	private Node3D _cameraPivot;
 	
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		_cameraPivot = GetChildren().OfType<Node3D>().FirstOrDefault(d => d.Name == "CameraPivot");
-	}
-	
-	const float MouseSensitivity = 0.01f;
-	private static readonly float TiltLimit = Mathf.DegToRad(75);
+	public override void _Ready() =>
+		_cameraPivot = GetChildren()
+			.OfType<Node3D>()
+			.FirstOrDefault(d => d.Name == "CameraPivot");
 
+	const float MouseSensitivity = 0.01f;
+	private static readonly float TiltLimit = Mathf.DegToRad(180);
+	
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		//nicked from https://docs.godotengine.org/en/stable/tutorials/3d/spring_arm.html
@@ -24,9 +24,9 @@ public partial class Player : CharacterBody3D
 		var x = _cameraPivot.Rotation.X;
 		x -= mouseMotion.Relative.Y * MouseSensitivity;
 		x = Mathf.Clamp(x, -TiltLimit, TiltLimit);
-		_cameraPivot.Rotation = new Vector3(x, -mouseMotion.Relative.X * MouseSensitivity, _cameraPivot.Rotation.Z);
-
-
+		var y = _cameraPivot.Rotation.Y;
+		y += -mouseMotion.Relative.X * MouseSensitivity; 
+		_cameraPivot.Rotation = new Vector3(x, y, _cameraPivot.Rotation.Z);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
